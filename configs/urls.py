ff93c674d4958c89f596946b3d7295cc7b0eca69
aisclass <aisclass@ukr.net>
2024-04-30 06:59:19 +0300

    initial

---
+++ configs/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+# from cars.views import CarTestView, CarDetailsView
+from  cars.views import CarListCreateView
+
+urlpatterns = [
+    # path('carsTest', CarTestView.as_view()),
+    # path('carsDetail/<int:pk>', CarDetailsView.as_view()),
+    # path('carsDetail/<str:pk>', CarDetailsView.as_view()),
+    # path('carsDetail/<str:pk>', CarDetailsView.as_view()), # строка без спецсимволов, проходят буквы цифры и _
+    path('cars', CarListCreateView.as_view())
+]
