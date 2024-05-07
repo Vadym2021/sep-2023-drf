1c8b41434609e80383b1c00fb1287ed7ae2c3b5a
aisclass <aisclass@ukr.net>
2024-05-07 08:59:25 +0300

    lesson 03 done + lesson 04

--- apps/cars/filters.py
+++ apps/cars/filters.py
@@ -7,33 +7,43 @@ from apps.cars.models import CarModel
 
 def car_filter(query: QueryDict) -> QuerySet:
     qs = CarModel.objects.all()
-    sort_by = query.get('sort_by')
+
+    filters = {}
+    sort_by = None
+
+    for k, v in query.items():
+        match k:
+            case 'price_gt':
+                filters['price__gt'] = v
+            case 'price_lt':
+                filters['price__lt'] = v
+            case 'year__gte':
+                filters['year__gte'] = v
+            case 'year__lte':
+                filters['year__lte'] = v
+            case 'brand_starts_with':
+                filters['brand__startswith'] = v
+            case 'brand_ends_with':
+                filters['brand__endswith'] = v
+            case 'brand_contains':
+                filters['brand__icontains'] = v
+            case 'sort_by':
+                sort_by = v
+            case _:
+                raise ValidationError(f"{k} is not a valid filter")
+
+
+    for field, value in filters.items():
+        qs = qs.filter(**{field: value})
+
+
     if sort_by:
         if sort_by.startswith('-'):
             sort_field = sort_by[1:]
             qs = qs.order_by(f'-{sort_field}')
         else:
             qs = qs.order_by(sort_by)
-        return qs
-
-    for k, v in query.items():
-        match k:
-            case 'price_gt':
-                qs = qs.filter(price__gt=v)
-            case 'price_lt':
-                qs = qs.filter(price__lt=v)
-            case 'year__gte':
-                qs = qs.filter(year__gte=v)
-            case 'year__lte':
-                qs = qs.filter(year__lte=v)
-            case 'brand_starts_with':
-                qs = qs.filter(brand__startswith=v)
-            case 'brand_ends_with':
-                qs = qs.filter(brand__endswith=v)
-            case 'brand_contains':
-                qs = qs.filter(brand__icontains=v)
-            case _:
-                raise ValidationError(f"{k} is not a valid filter")
 
     return qs
+
 
